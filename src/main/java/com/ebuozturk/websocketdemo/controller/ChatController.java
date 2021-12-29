1f5dd6f3673fedd29826d644106b0fc4ce19ba01
ebuozturk <ebuozturk98@gmail.com>
2021-12-30 01:37:44 +0300

    first commit

---
+++ src/main/java/com/ebuozturk/websocketdemo/controller/ChatController.java
@@ -0,0 +1,26 @@
+package com.ebuozturk.websocketdemo.controller;
+
+import org.springframework.messaging.handler.annotation.MessageMapping;
+import org.springframework.messaging.handler.annotation.Payload;
+import org.springframework.messaging.simp.SimpMessagingTemplate;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.CrossOrigin;
+
+@Controller
+@CrossOrigin
+public class ChatController {
+
+    private final SimpMessagingTemplate messagingTemplate;
+
+    public ChatController(SimpMessagingTemplate messagingTemplate) {
+        this.messagingTemplate = messagingTemplate;
+    }
+
+    @MessageMapping("/chat")
+    public void chatEndpoint(@Payload WsMessage wsMessage){
+        System.out.println(wsMessage);
+        messagingTemplate.convertAndSend("/topic",wsMessage);
+    }
+
+
+}
